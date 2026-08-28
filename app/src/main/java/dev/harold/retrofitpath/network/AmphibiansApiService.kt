4bafa91edcb9d6fd8488032374cb2d1c39f9d575
Harold Camacho Herrera <haroldherrera2001@outlook.com>
2026-08-27 23:57:11 -0600

    Elimina aplicacion del codelab

--- app/src/main/java/dev/harold/retrofitpath/network/AmphibiansApiService.kt
+++
@@ -1,9 +0,0 @@
-package dev.harold.retrofitpath.network
-
-import dev.harold.retrofitpath.model.AmphibianInfo
-import retrofit2.http.GET
-
-interface AmphibiansApiService {
-    @GET("amphibians")
-    suspend fun getAmphibians(): List<AmphibianInfo>
-}
